7525c1c93fc0ffc0b4d59fa2dcf87886be273ef9
Anurag473 <anurag47mishra@gmail.com>
2024-09-22 23:21:54 +0530

    added email  service

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,56 +1,80 @@
-import React, { useContext, useState } from 'react'
-import MainHeader from '../MainHeader/MainHeader'
-import Styles from './Contact.module.css'
-import Paragraph from '../paragraph/Paragraph'
-import { contextMode } from '../AppLayout/AppLayout'
-import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import Styles from './Contact.module.css';
+import Paragraph from '../paragraph/Paragraph';
+import { contextMode } from '../AppLayout/AppLayout';
+import emailjs from 'emailjs-com';
 
 
 export default function Contact() {
-  const {mode,profile}=useContext(contextMode)
-  const [name,setName]=useState('')
-  const [email,setEmail]=useState('')
-  const [message,setMessage]=useState('')
-  const recieverEmail=profile.email
-  const subject="Let's Collabrate"
+  const { mode, profile } = useContext(contextMode);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    try {
-      await axios.post('https://pexiflow-backend.onrender.com/api/contact/sendEmail', { name, email, message,recieverEmail,subject});
+    const templateParams = {
+      name,
+      email,
+      message,
+    };
 
-      setName('');
-      setEmail('');
-      setMessage('');
-    } catch (error) {
-      alert('something went wrong')
-    }
+    emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      templateParams,
+      import.meta.env.VITE_EMAILJS_USER_ID)
+      .then((response) => {
+        console.log('Email sent successfully!', response.status, response.text);
+        alert('Message sent successfully!'); // Notify user
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        alert('Something went wrong, please try again.');
+      });
   };
 
   return (
     <main className={Styles.container}>
-    <h2 style={{backgroundColor:mode==='dark'?'rgb(5,5,5)':'#f3f4f6',color:mode==='dark'?'#f3f4f6':'black'}} className={Styles.header}>Let's Connect</h2>
-    <div className={Styles.gridContainer}>
+      <h2 style={{ backgroundColor: mode === 'dark' ? 'rgb(5,5,5)' : '#f3f4f6', color: mode === 'dark' ? '#f3f4f6' : 'black' }} className={Styles.header}>Let's Connect</h2>
+      <div className={Styles.gridContainer}>
         <div className={Styles.contact}>
-            <h5 style={{backgroundColor:mode==='dark'?'rgb(5,5,5)':'#f3f4f6',color:mode==='dark'?'#f3f4f6':'black'}} className={Styles.messageMe}>Message me</h5>
-            <input value={name} onChange={(e)=>{setName(e.target.value)}} placeholder='Name' className={Styles.nameInput} type='text'/>
-            <input value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='Email' className={Styles.emailInput} type='email'/>
-            <textarea value={message} onChange={(e)=>{setMessage(e.target.value)}} placeholder='Write a message...' className={Styles.messageInput} type='text'/>
-            <button className={Styles.btttn} onClick={(e)=>handleSubmit(e)}>Send Message</button>
+          <h5 style={{ backgroundColor: mode === 'dark' ? 'rgb(5,5,5)' : '#f3f4f6', color: mode === 'dark' ? '#f3f4f6' : 'black' }} className={Styles.messageMe}>Message me</h5>
+          <input value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' className={Styles.nameInput} type='text' required />
+          <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' className={Styles.emailInput} type='email' required />
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Write a message...' className={Styles.messageInput} required />
+          <button className={Styles.btttn} onClick={handleSubmit}>Send Message</button>
         </div>
         <div className={Styles.contactInfo}>
-        <h5 style={{backgroundColor:mode==='dark'?'rgb(5,5,5)':'#f3f4f6',color:mode==='dark'?'#f3f4f6':'black'}} className={Styles.contactHeader}>Contact</h5>
-        <Paragraph><ion-icon style={{fontSize:'1.8rem',paddingRight:'0.8rem'}} name="mail-outline"></ion-icon><span className={Styles.small}>anurag47mishra@gmail.com</span></Paragraph>
-        <Paragraph><ion-icon style={{fontSize:'1.8rem',paddingRight:'0.8rem'}} name="call-outline"></ion-icon><span className={Styles.small}>+91 1234567890</span></Paragraph>
+          <h5 style={{ backgroundColor: mode === 'dark' ? 'rgb(5,5,5)' : '#f3f4f6', color: mode === 'dark' ? '#f3f4f6' : 'black' }} className={Styles.contactHeader}>Contact</h5>
+          <Paragraph>
+            <ion-icon style={{ fontSize: '1.8rem', paddingRight: '0.8rem' }} name="mail-outline"></ion-icon>
+            <span className={Styles.small}>your-email@gmail.com</span>
+          </Paragraph>
+          <Paragraph>
+            <ion-icon style={{ fontSize: '1.8rem', paddingRight: '0.8rem' }} name="call-outline"></ion-icon>
+            <span className={Styles.small}>+91 1234567890</span>
+          </Paragraph>
         </div>
         <div className={Styles.contactInfo}>
-        <h5 style={{backgroundColor:mode==='dark'?'rgb(5,5,5)':'#f3f4f6',color:mode==='dark'?'#f3f4f6':'black'}} className={Styles.contactHeader}>Social Media</h5>
-        <Paragraph><ion-icon style={{fontSize:'1.8rem',paddingRight:'0.8rem'}} name="logo-linkedin"></ion-icon><span className={Styles.small}>LinkedIn</span></Paragraph>
-        <Paragraph><ion-icon style={{fontSize:'1.8rem',paddingRight:'0.8rem'}} name="logo-instagram"></ion-icon><span className={Styles.small}>Instagram</span></Paragraph>
-        <Paragraph><ion-icon style={{fontSize:'1.8rem',paddingRight:'0.8rem'}} name="logo-github"></ion-icon><span className={Styles.small}>Github</span></Paragraph>
+          <h5 style={{ backgroundColor: mode === 'dark' ? 'rgb(5,5,5)' : '#f3f4f6', color: mode === 'dark' ? '#f3f4f6' : 'black' }} className={Styles.contactHeader}>Social Media</h5>
+          <Paragraph>
+            <ion-icon style={{ fontSize: '1.8rem', paddingRight: '0.8rem' }} name="logo-linkedin"></ion-icon>
+            <span className={Styles.small}>LinkedIn</span>
+          </Paragraph>
+          <Paragraph>
+            <ion-icon style={{ fontSize: '1.8rem', paddingRight: '0.8rem' }} name="logo-instagram"></ion-icon>
+            <span className={Styles.small}>Instagram</span>
+          </Paragraph>
+          <Paragraph>
+            <ion-icon style={{ fontSize: '1.8rem', paddingRight: '0.8rem' }} name="logo-github"></ion-icon>
+            <span className={Styles.small}>GitHub</span>
+          </Paragraph>
         </div>
-    </div>
+      </div>
     </main>
-  )
+  );
 }
